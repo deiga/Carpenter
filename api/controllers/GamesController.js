36a03a79f6f70319cba808587bd24a418a375808
Timo Sand <timo.j.sand@gmail.com>
2014-03-09 23:49:05 +0200

    Removed comments

--- api/controllers/GamesController.js
+++ api/controllers/GamesController.js
@@ -1,30 +1,8 @@
 var after = require('after');
 
-/**
- * GamesController
- *
- * @module      :: Controller
- * @description	:: A set of functions called `actions`.
- *
- *                 Actions contain code telling Sails how to respond to a certain type of request.
- *                 (i.e. do stuff, then send some JSON, show an HTML page, or redirect to another URL)
- *
- *                 You can configure the blueprint URLs which trigger these actions (`config/controllers.js`)
- *                 and/or override them with custom routes (`config/routes.js`)
- *
- *                 NOTE: The code you write here supports both HTTP and Socket.io automatically.
- *
- * @docs        :: http://sailsjs.org/#!documentation/controllers
- */
- var GamesController = {};
+var GamesController = {};
 
-/**
- * Action blueprints:
- *    `/games/common`
- *
- * get common games of list of users, return as JSON for now
- */
- GamesController.common = function (req, res) {
+GamesController.common = function (req, res) {
   var user_ids = [];
   if (typeof req.params.ids === 'undefined') {
     user_ids = req.query.users.split(',').map(function(id) { return id.trim(); });
@@ -89,9 +67,5 @@ function finish(res, err, games) {
 }
 
 
-/**
- * Overrides for the settings in `config/controllers.js`
- * (specific to GamesController)
- */
- GamesController._config = {};
- module.exports = GamesController;
+GamesController._config = {};
+module.exports = GamesController;
--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -6,22 +6,6 @@ var relyingPart = new openid.RelyingParty(
   false,
   []);
 
-/**
- * UsersController
- *
- * @module      :: Controller
- * @description	:: A set of functions called `actions`.
- *
- *                 Actions contain code telling Sails how to respond to a certain type of request.
- *                 (i.e. do stuff, then send some JSON, show an HTML page, or redirect to another URL)
- *
- *                 You can configure the blueprint URLs which trigger these actions (`config/controllers.js`)
- *                 and/or override them with custom routes (`config/routes.js`)
- *
- *                 NOTE: The code you write here supports both HTTP and Socket.io automatically.
- *
- * @docs        :: http://sailsjs.org/#!documentation/controllers
- */
 var UsersController = {};
 
 UsersController.callback = function(req, res) {
@@ -84,12 +68,6 @@ UsersController.show = function (req, res) {
 
 };
 
-
-  /**
-   * Overrides for the settings in `config/controllers.js`
-   * (specific to UserController)
-   */
-  _config: {}
 
 UsersController._config = {};
 
