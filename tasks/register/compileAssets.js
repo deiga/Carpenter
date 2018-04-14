b50ea2099dd48ba43536ff1997f682e1964654c0
Timo Sand <timo.sand@iki.fi>
2018-04-14 14:57:51 +0200

    Removes requirement on Bower

--- tasks/register/compileAssets.js
+++ tasks/register/compileAssets.js
@@ -1,7 +1,6 @@
 module.exports = function (grunt) {
     grunt.registerTask('compileAssets', [
         'clean:dev',
-        'bower:dev',
         'jst:dev',
         'less:dev',
         'copy:dev',
